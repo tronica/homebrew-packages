91dde64cefcb114a72fd77c052e307c2c1afd06d
Patrick <1822532+patrixr@users.noreply.github.com>
2025-01-28 13:59:51 +0800

    Update formula

--- Formula/auteur.rb
+++ Formula/auteur.rb
@@ -1,11 +1,8 @@
 class Auteur < Formula
   desc "Static page generator"
   homepage "https://auteur.tronica.io"
-  url "https://github.com/patrixr/auteur/archive/refs/tags/v0.0.17.tar.gz"
-  sha256 "af46282c608f7c2fdd5fdd911ab38b69e7d5c1db74aaeca72a34651babf5b9d1"
   license "NOASSERTION"
   head "https://github.com/patrixr/auteur.git"
-  depends_on "go" => :build
   versiom "0.0.17"
 
 
