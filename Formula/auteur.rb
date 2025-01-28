8ba29a52951fbd8b4b7cf0fbb9dc35623d56fa45
Patrick <1822532+patrixr@users.noreply.github.com>
2025-01-28 13:49:27 +0800

    Update formula

--- Formula/auteur.rb
+++ Formula/auteur.rb
@@ -1,9 +1,6 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Auteur < Formula
   desc "Static page generator"
-  homepage ""
+  homepage "https://auteur.tronica.io"
   url "https://github.com/patrixr/auteur/archive/refs/tags/v0.0.17.tar.gz"
   sha256 "af46282c608f7c2fdd5fdd911ab38b69e7d5c1db74aaeca72a34651babf5b9d1"
   license "NOASSERTION"
@@ -12,14 +9,14 @@ class Auteur < Formula
 
 
   def install
-    ENV["GOPATH"] = buildpath
+    cd buildpath do
+      # Install Go dependencies
+      system "make", "tidy"
 
-    # Install Go dependencies
-    system "make", "tidy"
-
-    # Build and install auteur
-    system "make", "build"
-    bin.install "out/auteur"
+      # Build and install auteur
+      system "make", "build"
+      bin.install "out/auteur"
+    end
   end
 
   test do
