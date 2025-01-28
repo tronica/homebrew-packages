d7c9a17a324707a9fb4735c4635854c017767b2d
Patrick <1822532+patrixr@users.noreply.github.com>
2025-01-28 13:55:31 +0800

    Update formula

--- Formula/auteur.rb
+++ Formula/auteur.rb
@@ -6,18 +6,29 @@ class Auteur < Formula
   license "NOASSERTION"
   head "https://github.com/patrixr/auteur.git"
   depends_on "go" => :build
+  versiom "0.0.17"
 
 
-  def install
-    cd buildpath do
-      # Install Go dependencies
-      system "make", "tidy"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/patrixr/auteur/releases/download/v#{version}/auteur-v#{version}-darwin-arm64.tar.gz"
+      sha256 "e6cc596eddec67fc6c2dd98a019b96986d2933560e4f4f362dc73593ee624ddf"
 
-      # Build and install auteur
-      system "make", "build"
-      bin.install "out/auteur"
+      def install
+          bin.install "auteur" => "auteur"
+      end
+    end
+
+    if Hardware::CPU.intel?
+      url "https://github.com/patrixr/auteur/releases/download/v#{version}/auteur-v#{version}-darwin-amd64.tar.gz"
+      sha256 "9a89a0749dfa376e01a720535610ed5cc12d72e2ab243f3b33cda4579413b94a"
+
+      def install
+          bin.install "auteur" => "auteur"
+      end
     end
   end
+
 
   test do
     system "#{bin}/auteur", "--help"
