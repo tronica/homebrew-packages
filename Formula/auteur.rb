4304cab4ffbe2446ce1a8cb9e30478cd9af6579c
Patrick <1822532+patrixr@users.noreply.github.com>
2025-02-19 14:22:56 +0800

    Update auteur to v0.0.23

--- Formula/auteur.rb
+++ Formula/auteur.rb
@@ -3,27 +3,27 @@ class Auteur < Formula
   homepage "https://auteur.tronica.io"
   license "NOASSERTION"
   head "https://github.com/patrixr/auteur.git"
-  version "0.0.22"
+  version "0.0.23"
 
   # Checksum command
-  # curl -L https://github.com/patrixr/auteur/releases/download/v0.0.X/auteur-v0.0.X-darwin-arm64.tar.gz | shasum -a 256
+  # curl -L https://github.com/patrixr/auteur/releases/download/v0.0.23/auteur-v0.0.23-darwin-arm64.tar.gz | shasum -a 256
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/patrixr/auteur/releases/download/v#{version}/auteur-v#{version}-darwin-arm64.tar.gz"
-      sha256 "db504972945ff4b9b3533bc5ecca4f64d94a044575b01e5b84103ad5631ec685"
+      sha256 "0ef0bc8670e8abf0a3f6d8e13fa4c7d3bf29c1cbd61f38132f33682722476b8c"
 
       def install
-          bin.install "auteur" => "auteur"
+        bin.install "auteur" => "auteur"
       end
     end
 
     if Hardware::CPU.intel?
       url "https://github.com/patrixr/auteur/releases/download/v#{version}/auteur-v#{version}-darwin-amd64.tar.gz"
-      sha256 "6cc8c26d2f178874fc4d15b481f01fa9339ab1ead25ec75da0bae822f9722e6e"
+      sha256 "ced6505dfcc415b37c227981979a08d9a3f6bba999f335d3aa2211a23e4e225d"
 
       def install
-          bin.install "auteur" => "auteur"
+        bin.install "auteur" => "auteur"
       end
     end
   end
